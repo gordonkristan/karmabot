b42c53f1f6819babf5e5a58c343e3756bcaaf662
Gordon Kristan <gordon@sproutsocial.com>
2016-04-29 14:30:18 -0500

    Bugfix

--- scripts/karma.js
+++ scripts/karma.js
@@ -3,25 +3,19 @@ var pg = require('pg');
 var insertKarma = function(subject, amount, callback) {
 	pg.connect(process.env.DATABASE_URL, function(error, client, done) {
 		if (error) {
-			console.log(7);
 			callback(error);
 			done();
 		} else {
-			console.log(8);
 			client.query('INSERT INTO karma VALUES ($1, $2)', [subject, amount], function(error, result) {
 				if (error) {
-					console.log(9);
 					callback(error);
 					done();
 				} else {
-					console.log(10);
 					client.query('SELECT COALESCE(SUM(karma_change), 0) as total_karma FROM karma WHERE subject = $1', [subject], function(error, result) {
 						if (error) {
-							console.log(11);
 							callback(error);
 							done();
 						} else {
-							console.log(12);
 							callback(null, result.rows[0].total_karma);
 							done();
 						}
@@ -35,10 +29,8 @@ var insertKarma = function(subject, amount, callback) {
 module.exports = function(robot) {
 
 	robot.hear(/(\S+)(:\s?)?(\+\+|\-\-)/i, function(response) {
-		console.log(1);
-		var operation = response.match[2];
+		var operation = response.match[3];
 		if (operation !== '++' && operation !== '--') {
-			console.log(2);
 			return;
 		}
 
@@ -49,19 +41,14 @@ module.exports = function(robot) {
 			isUsername = true;
 		}
 
-		console.log(3);
-
 		insertKarma(subject, (operation === '++' ? 1 : -1), function(error, newValue) {
 			if (error) {
-				console.log(4);
 				return;
 			}
 
-			console.log(5);
 			var verb = (operation === '++' ? 'risen' : 'fallen');
 			var replySubject = (isUsername = '@' ? subject : subject);
 			response.send(replySubject +'\'s karma has ' + verb + ' to ' + (newValue || 0));
-			console.log(6);
 		});
 	});
 
