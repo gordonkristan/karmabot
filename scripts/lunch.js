2111d37a3ca581b8aee4712ef3470cfe35f5dc88
Henry Millison <hmillison@gmail.com>
2016-06-03 12:53:57 -0500

    adding more search strings

--- scripts/lunch.js
+++ scripts/lunch.js
@@ -4,4 +4,7 @@ var handleLunchOffer = function(response) {
 
 module.exports = function(robot) {
 	robot.hear(/up for grabs/i, handleLunchOffer);
+	robot.hear(/you can have/i, handleLunchOffer);
+	robot.hear(/out sick/i, handleLunchOffer);
+	robot.hear(/WFH/i, handleLunchOffer);
 };
