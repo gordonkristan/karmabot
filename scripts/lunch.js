8b66ab9b7b58f5d0b35e35302a7912e6df2c4552
Henry Millison <hmillison@gmail.com>
2016-06-14 15:27:13 -0500

    weigh respopnses based on confidence level

--- scripts/lunch.js
+++ scripts/lunch.js
@@ -1,10 +1,23 @@
+var messageConfidenceHash = {
+	'up for grabs' : .8,
+	'you can have' : .6,
+	'out sick'     : .5,
+	'wfh'		   : .3 
+}
+
+var shouldRobotRespond = function(message) {
+	var confidenceLevel = messageConfidenceHash[message.toLowerCase()];
+	if (!confidenceLevel) {
+		return false;
+	}
+	var random = Math.random();
+	return confidenceLevel <= random; 
+}
+
 var handleLunchOffer = function(response) {
 	response.send(' ooooooooo :fiestaparrot: :tayne: :parrotdad: :donkeysauce: :chompy:')
 };
 
 module.exports = function(robot) {
-	robot.hear(/up for grabs/i, handleLunchOffer);
-	robot.hear(/you can have/i, handleLunchOffer);
-	robot.hear(/out sick/i, handleLunchOffer);
-	robot.hear(/WFH/i, handleLunchOffer);
+	robot.listen(shouldRobotRespond, handleLunchOffer);
 };
